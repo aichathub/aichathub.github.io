add955b18f5015018eb58a64d12305ea39a70100
aichathub <126531734+aichathub@users.noreply.github.com>
2023-04-20 14:04:03 -0400

    Further increase tolerance on message input

--- src/components/MessageInput.tsx
+++ src/components/MessageInput.tsx
@@ -152,7 +152,7 @@ export const MessageInput: React.FC<{
   </Tooltip>;
   useEffect(() => {
     const onScroll = () => {
-      const tolerance = 25;
+      const tolerance = 50;
       setIsAtBottom(window.innerHeight + window.scrollY + tolerance >= document.documentElement.scrollHeight);
     }
     window.removeEventListener("scroll", onScroll);
