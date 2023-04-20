71b776b4bbffae305e5effa6457f2304fd8cb78b
lwy123177 <118701490+lwy123177@users.noreply.github.com>
2023-04-20 18:27:56 -0400

    Update AgentDialog.tsx

--- src/components/AgentDialog.tsx
+++ src/components/AgentDialog.tsx
@@ -65,7 +65,7 @@ const AgentDialog: React.FC<{
         TransitionComponent={Transition}
       >
         <FormControl
-          style={{ width: "20%" }}
+          style={{ width: "30vh" }}
         >
           <Select
             label="Agent"
