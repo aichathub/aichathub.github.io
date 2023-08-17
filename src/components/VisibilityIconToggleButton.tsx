9ac519df4450d3f4083d11a8b63ed862bef2a852
aichathub <aichathub@outlook.com>
2023-08-16 20:41:40 -0400

    Add ToolTip for secret element

--- src/components/VisibilityIconToggleButton.tsx
+++ src/components/VisibilityIconToggleButton.tsx
@@ -1,12 +1,14 @@
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import { IconButton } from "@mui/material";
+import { IconButton, Tooltip } from "@mui/material";
 import { useState } from "react";
 
 const VisibilityIconToggleButton = () => {
   const [isOpen, setIsOpen] = useState(false);
-  return <IconButton aria-label="view more" onClick={() => { setIsOpen(prev => !prev) }}>
-    {isOpen ? <VisibilityOffIcon /> : <VisibilityIcon />}
-  </IconButton>;
+  return <Tooltip title={isOpen ? "Hide" : "Read More"}>
+    <IconButton aria-label="view more" onClick={() => { setIsOpen(prev => !prev) }}>
+      {isOpen ? <VisibilityOffIcon /> : <VisibilityIcon />}
+    </IconButton>
+  </Tooltip>;
 }
 export default VisibilityIconToggleButton;
