e9c703569b02c2c8f78fb3367e7a04a6015ff938
aichathub <126531734+aichathub@users.noreply.github.com>
2023-08-29 04:46:35 -0400

    Change to spotify compact player

--- src/components/MarkdownComponent.tsx
+++ src/components/MarkdownComponent.tsx
@@ -73,7 +73,7 @@ const MarkdownComponent: React.FC<{
             res = <>
               {res}
               <div>
-                <iframe style="border-radius:12px" src={`https://open.spotify.com/embed/track/${spotifyId}?utm_source=generator${context.darkMode ? "&theme=0" : ""}`} width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <iframe style="border-radius:12px" src={`https://open.spotify.com/embed/track/${spotifyId}?utm_source=generator${context.darkMode ? "&theme=0" : ""}`} width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
               </div>
             </>
           }
