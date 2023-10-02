9560394444a8edd05e000de5727a2b200731a059
aichathub <aichathub@outlook.com>
2023-10-02 18:45:24 -0400

    Change Agent Dialog wording

--- src/components/AgentDialog.tsx
+++ src/components/AgentDialog.tsx
@@ -88,8 +88,8 @@ const AgentDialog: React.FC<{
           <MenuItem value={"gpt3.5"}>ChatGPT (gpt-3.5-turbo)</MenuItem>
           <MenuItem value={"gpt4"}>ChatGPT (gpt-4)</MenuItem>
           <MenuItem value={"llama70b"}>LLaMA 2 70B</MenuItem>
-          <MenuItem value={"yourmodel"}>Your Model</MenuItem>
-          <MenuItem value={"python"}>Python</MenuItem>
+          <MenuItem value={"yourmodel"}>Your Model (Google Colab)</MenuItem>
+          <MenuItem value={"python"}>Python (Runtime)</MenuItem>
         </Select>
         {agent === "yourmodel" && <TextField id="standard-basic"
           label="Your model api url"
