b6a9e591f31f1d6c0b3472994fa3dcb9f8fccd84
aichathub <aichathub@outlook.com>
2023-10-05 22:49:09 -0400

    Only show follow up button at bottom

--- src/store/AppContext.tsx
+++ src/store/AppContext.tsx
@@ -271,6 +271,7 @@ export const AppContextProvider: React.FC<{ children: ReactNode }> = (
   const [showLoadingBackdrop, setShowLoadingBackdrop] = useState(false);
   const [showQrReader, setShowQrReader] = useState(false);
   const [detectedSessionid, setDetectedSessionid] = useState("");
+  const [isAtBottom, setIsAtBottom] = useState(false);
 
   const isOnPostPage = useMatch("/:username/:postid");
   const hasRightToSendMsg = isOnPostPage && curPost && curPost.username === loggedUser;
@@ -660,6 +661,16 @@ export const AppContextProvider: React.FC<{ children: ReactNode }> = (
     refreshKeywords(loggedUser, auth.token);
   }, [loggedUser, auth.token]);
 
+  useEffect(() => {
+    const onScroll = () => {
+      const tolerance = 50;
+      setIsAtBottom(window.innerHeight + window.scrollY + tolerance >= document.documentElement.scrollHeight);
+    }
+    window.removeEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
   const getSeverity = (message: string) => {
     if (message.toLowerCase().indexOf("error") !== -1) return "error";
     if (message.toLowerCase().indexOf("success") !== -1) return "success";
@@ -716,7 +727,7 @@ export const AppContextProvider: React.FC<{ children: ReactNode }> = (
   });
 
   const showInputMessage = hasRightToSendMsg && !isLoadingMessages;
-  const showAskFollowupButton = !showInputMessage && isOnPostPage && curPost;
+  const showAskFollowupButton = !showInputMessage && isOnPostPage && curPost && isAtBottom;
 
   const mainBody = <Box sx={{ display: isInitializing ? "none" : "flex", marginBottom: "30px" }}>
     <CssBaseline />
