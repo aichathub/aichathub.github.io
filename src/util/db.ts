c5df8be53f97a74b9cabc2f5a6407da99cb8d314
lwy123177 <118701490+lwy123177@users.noreply.github.com>
2023-10-11 13:37:45 -0400

    Avoid title being too long

--- src/util/db.ts
+++ src/util/db.ts
@@ -115,6 +115,10 @@ export const deleteMessageByMid = async (mid: number, token: string, username: s
 }
 
 export const insertPostByUsernameAndTitle = async (username: string, title: string, token: string, tags: TagModel[], isprivate: boolean) => {
+  const postTitleLengthLimit = 100;
+  if (title.length > postTitleLengthLimit) {
+    title = title.substring(0, postTitleLengthLimit) + "...";
+  }
   const response = await fetch(`${backendServer}/api/insert/post/`, {
     method: "POST",
     headers: {
