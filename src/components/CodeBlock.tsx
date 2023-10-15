1f077e0d79b008c8ef7a7d81e71ef958285c8455
aichathub <aichathub@outlook.com>
2023-10-14 21:36:24 -0400

    Update highlight theme based on dark/light mode

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,13 +1,19 @@
+import { useContext } from "react";
 import Highlight from "react-highlight";
+import { AppContext } from "../store/AppContext";
+import "./AtomOneDark.css";
+import "./AtomOneLight.css";
 import CopyWrapper from "./CopyWrapper";
+
 
 const CodeBlock: React.FC<{
   content: string;
   language?: string;
 }> = (props) => {
+  const context = useContext(AppContext);
   return (
     <CopyWrapper content={props.content}>
-      <Highlight className={props.language}>{props.content.trim()}</Highlight>
+      <Highlight className={`${props.language} ${context.darkMode ? "onedark" : "onelight"}`}>{props.content.trim()}</Highlight>
     </CopyWrapper>
   );
 };
