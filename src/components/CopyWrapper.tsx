1555bae3ac08bb7e0ae176e97837171c19c984c1
lwy123177 <118701490+lwy123177@users.noreply.github.com>
2023-10-16 12:50:29 -0400

    Improve codeblock style (2)

--- src/components/CopyWrapper.tsx
+++ src/components/CopyWrapper.tsx
@@ -2,6 +2,9 @@ import { Grid } from "@mui/material";
 import CopyButton from "./CopyButton";
 import classes from "./CopyWrapper.module.css";
 import EditButton from "./EditButton";
+import { AppContext } from "../store/AppContext";
+import { useContext } from "react";
+
 const CopyWrapper: React.FC<{
   content: string;
   children: JSX.Element;
@@ -9,9 +12,10 @@ const CopyWrapper: React.FC<{
   setIsEditing?: (isEditing: boolean) => void;
   hasRightToEdit?: boolean;
 }> = (props) => {
+  const context = useContext(AppContext);
   return (
     <>
-      <Grid style={{ position: "relative" }} className={classes['horizontal-scroll']}>
+      <Grid style={{ position: "relative" }} className={`${classes['horizontal-scroll']} ${context.darkMode ? classes['dark'] : classes['light']}`}>
         <CopyButton content={props.content} />
         {
           props.hasRightToEdit && !props.isEditing && props.setIsEditing && <EditButton setIsEditing={props.setIsEditing} />
