6eb184f09c0bcfa96f6fe59b5046529a627d903e
aichathub <126531734+aichathub@users.noreply.github.com>
2023-10-17 21:33:05 -0400

    Update to use runpy

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -8,5 +8,6 @@ export const AI_USERNAME = "ai";
 export const GOOGLE_LOGIN_CLIENT_ID = "544615908375-24otdb6t0v54un3b1n2b57iflovhj9no.apps.googleusercontent.com";
 export const GITHUB_LOGIN_CLIENT_ID = "dbc1cef903c0e90e1033";
 export const LLAMA_70B_API = "https://welllitappropriatejavabytecode.aichathubapp.repl.co/api/llama70b";
-export const PYTHON_RUNTIME_API = "https://roundedidledecompiler.aichathubapp.repl.co/api/execute";
+// export const PYTHON_RUNTIME_API = "https://roundedidledecompiler.aichathubapp.repl.co/api/execute";
+export const PYTHON_RUNTIME_API = "https://roundedidledecompiler.aichathubapp.repl.co/api/runpy";
 export const GUEST_POST_LIMIT = 10;
